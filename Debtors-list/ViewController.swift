efbd3d4251f8b88cd1c33cf07d524e2375f36aef
Vlad Lytvynets <vlad.lytvynets@icloud.com>
2022-03-17 19:15:19 +0200

    Initial commit

---
+++ Debtors-list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Debtors-list
+//
+//  Created by Vlad Lytvynets on 17.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
